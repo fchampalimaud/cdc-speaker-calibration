a7f00e5620be833bfe19654136ae47ee58a5fa2c
José Grilo <josegrilo.q1b@gmail.com>
2024-07-02 21:54:07 +0100

    first gui layout finished

--- speaker_calibration/gui_test.py
+++ speaker_calibration/gui_test.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 import matplotlib
+import ctypes
 # import matplotlib.pyplot as plt
 
 matplotlib.use("TkAgg")
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+
+myappid = "mycompany.myproduct.subproduct.version"  # arbitrary string
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 
 class PlotFrame(ttk.Frame):
@@ -45,23 +49,30 @@ class OptionsFrame(ttk.Frame):
         self.config_window = ConfigurationWindow()
 
         self.grid_columnconfigure(0, weight=1)
-        for i in range(4):
+        for i in range(6):
             self.grid_rowconfigure(i, weight=1)
 
-        # label
-        self.hardware_frame = HardwareFrame(self)
-        self.hardware_frame.grid(column=0, row=0)
-
-        self.combobox = ttk.Combobox(self)
-        self.combobox.grid(column=0, row=1)
-
-        # button
-        self.config_button = ttk.Button(self, text="Open Configuration Window", command=self.config_window.deiconify)
-        self.config_button.grid(column=0, row=2)
+        self.logo = tk.PhotoImage(file="assets/cf_logo.png")
+        self.logo_label = tk.Label(self, image=self.logo)
+        self.logo_label.grid(column=0, row=0)
 
         # button
         self.run_button = ttk.Button(self, text="Run")
-        self.run_button.grid(column=0, row=3)
+        self.run_button.grid(column=0, row=1)
+
+        self.combobox = ttk.Combobox(self)
+        self.combobox.grid(column=0, row=2)
+
+        # button
+        self.config_button = ttk.Button(self, text="Open Configuration Window", command=self.config_window.deiconify)
+        self.config_button.grid(column=0, row=3)
+
+        # label
+        self.hardware_frame = HardwareFrame(self)
+        self.hardware_frame.grid(column=0, row=4)
+
+        self.test_frame = TestFrame(self)
+        self.test_frame.grid(column=0, row=5)
 
 
 class HardwareFrame(ttk.LabelFrame):
@@ -69,7 +80,7 @@ class HardwareFrame(ttk.LabelFrame):
         super().__init__(container, text=text)
 
         self.grid_columnconfigure(0, weight=1)
-        for i in range(3):
+        for i in range(4):
             self.grid_rowconfigure(i, weight=1)
 
         # Parameters Frame
@@ -94,17 +105,35 @@ class HardwareFrame(ttk.LabelFrame):
         self.fs_cb = ttk.Combobox(self.fs_frame, width=10)
         self.fs_cb.grid(column=1, row=0, sticky="w")
 
-        # self.harp_soundcard_cb = ttk.Checkbutton(self, text="Harp SoundCard")
-        # self.harp_soundcard_cb.grid(column=0, row=0)
+        self.soundcard_frame = tk.Frame(self)
+        self.soundcard_frame.grid(column=0, row=1, pady=5)
 
-        # self.sc_id_text = ttk.Entry(self)
-        # self.sc_id_text.grid(column=2, row=0)
+        self.harp_soundcard_cb = ttk.Checkbutton(self.soundcard_frame, text="Harp SoundCard")
+        self.harp_soundcard_cb.grid(column=0, row=0, padx=5)
 
-        # self.harp_amp_cb = ttk.Checkbutton(self, text="Harp Amp")
-        # self.harp_amp_cb.grid(column=0, row=1)
+        self.sc_id_frame = tk.Frame(self.soundcard_frame)
+        self.sc_id_frame.grid(column=1, row=0, padx=5)
 
-        # self.amp_id_text = ttk.Entry(self)
-        # self.amp_id_text.grid(column=1, row=1)
+        self.sc_id_label = tk.Label(self.sc_id_frame, text="SoundCard ID")
+        self.sc_id_label.grid(column=0, row=0, sticky="e")
+
+        self.sc_id_text = ttk.Entry(self.sc_id_frame, width=15)
+        self.sc_id_text.grid(column=1, row=0, sticky="w")
+
+        self.harp_amp_frame = tk.Frame(self)
+        self.harp_amp_frame.grid(column=0, row=2, pady=5)
+
+        self.harp_amp_cb = ttk.Checkbutton(self.harp_amp_frame, text="Harp Amp")
+        self.harp_amp_cb.grid(column=0, row=0, padx=5)
+
+        self.amp_id_frame = tk.Frame(self.harp_amp_frame)
+        self.amp_id_frame.grid(column=1, row=0, padx=5)
+
+        self.amp_id_label = tk.Label(self.amp_id_frame, text="Amp ID")
+        self.amp_id_label.grid(column=0, row=0, sticky="e")
+
+        self.amp_id_text = ttk.Entry(self.amp_id_frame, width=15)
+        self.amp_id_text.grid(column=1, row=0, sticky="w")
 
         # ID frame
         self.id_frame = tk.Frame(self)
@@ -129,6 +158,78 @@ class HardwareFrame(ttk.LabelFrame):
         self.setup_id_sb.grid(column=1, row=0, sticky="w")
 
 
+class TestFrame(ttk.LabelFrame):
+    def __init__(self, container, text="Test Calibration"):
+        super().__init__(container, text=text)
+
+        self.grid_columnconfigure(0, weight=1)
+        for i in range(5):
+            self.grid_rowconfigure(i, weight=1)
+
+        self.lim_frame = tk.Frame(self)
+        self.lim_frame.grid(column=0, row=0, pady=5)
+
+        self.min_frame = tk.Frame(self.lim_frame)
+        self.min_frame.grid(column=0, row=0, padx=10)
+
+        self.min_label = ttk.Label(self.min_frame, text="Min dB")
+        self.min_label.grid(column=0, row=0, sticky="e")
+
+        self.min_sb = ttk.Spinbox(self.min_frame, width=10)
+        self.min_sb.grid(column=1, row=0, sticky="w")
+
+        self.max_frame = tk.Frame(self.lim_frame)
+        self.max_frame.grid(column=1, row=0, padx=10)
+
+        self.max_label = ttk.Label(self.max_frame, text="Max dB")
+        self.max_label.grid(column=0, row=0, sticky="e")
+
+        self.max_sb = ttk.Spinbox(self.max_frame, width=10)
+        self.max_sb.grid(column=1, row=0, sticky="w")
+
+        self.steps_frame = tk.Frame(self)
+        self.steps_frame.grid(column=0, row=1, pady=5)
+
+        self.steps_label = ttk.Label(self.steps_frame, text="Steps")
+        self.steps_label.grid(column=0, row=0, sticky="e")
+
+        self.steps_sb = ttk.Spinbox(self.steps_frame, width=10)
+        self.steps_sb.grid(column=1, row=0, sticky="w")
+
+        self.par_frame = tk.Frame(self)
+        self.par_frame.grid(column=0, row=2, pady=5)
+
+        self.slope_frame = tk.Frame(self.par_frame)
+        self.slope_frame.grid(column=0, row=0, padx=10)
+
+        self.slope_label = ttk.Label(self.slope_frame, text="Slope")
+        self.slope_label.grid(column=0, row=0, sticky="e")
+
+        self.slope_sb = ttk.Spinbox(self.slope_frame, width=10)
+        self.slope_sb.grid(column=1, row=0, sticky="w")
+
+        self.intercept_frame = tk.Frame(self.par_frame)
+        self.intercept_frame.grid(column=1, row=0, padx=10)
+
+        self.intercept_label = ttk.Label(self.intercept_frame, text="Intercept")
+        self.intercept_label.grid(column=0, row=0, sticky="e")
+
+        self.intercept_sb = ttk.Spinbox(self.intercept_frame, width=10)
+        self.intercept_sb.grid(column=1, row=0, sticky="w")
+
+        self.inverse_filter_frame = tk.Frame(self)
+        self.inverse_filter_frame.grid(column=0, row=3, pady=5)
+
+        self.inverse_filter_button = ttk.Button(self.inverse_filter_frame, text="Load Filter")
+        self.inverse_filter_button.grid(column=0, row=0)
+
+        self.inverse_filter_label = ttk.Label(self.inverse_filter_frame, text="No Filter")
+        self.inverse_filter_label.grid(column=1, row=0)
+
+        self.test_button = ttk.Button(self, text="Run Test")
+        self.test_button.grid(column=0, row=4, pady=5)
+
+
 class ConfigurationWindow(tk.Toplevel):
     def __init__(self):
         super().__init__()
@@ -137,6 +238,7 @@ class ConfigurationWindow(tk.Toplevel):
         self.resizable(0, 0)
         self.protocol("WM_DELETE_WINDOW", self.withdraw)
         self.withdraw()
+        self.iconbitmap("docs/img/favicon.ico")
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
@@ -277,6 +379,7 @@ class App(tk.Tk):
 
         # configure the root window
         self.title("Speaker Calibration")
+        self.iconbitmap("docs/img/favicon.ico")
 
         # Get the screen width and height
         screen_width = self.winfo_screenwidth()
