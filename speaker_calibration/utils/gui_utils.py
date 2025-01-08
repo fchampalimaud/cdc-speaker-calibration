2349b1a4fe572df5df64d819c7d433828556f521
José Grilo <jose.grilo@research.fchampalimaud.org>
2025-01-08 15:35:56 +0000

    GUI aspect finished

--- speaker_calibration/utils/gui_utils.py
+++ speaker_calibration/utils/gui_utils.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
 import serial.tools.list_ports
-import numpy as np
 from typing import Union
 
 
@@ -15,72 +14,6 @@ def get_ports():
     port_strings.append("Refresh")
 
     return port_strings
-
-
-# TODO: modify to be a class instead of a function
-def spinbox_row(
-    container,
-    label_text,
-    default_value,
-    from_,
-    to,
-    increment,
-    row,
-    spinbox_width=10,
-    int_value: int = 0,
-):
-    label = tk.Label(container, text=label_text)
-    label.grid(row=row, column=0, sticky="e")
-
-    if int_value == 0:
-        spinbox_variable = tk.StringVar(container, str(default_value))
-    else:
-        spinbox_variable = tk.IntVar(container, int(default_value))
-
-    spinbox = ttk.Spinbox(
-        container,
-        from_=from_,
-        to=to,
-        increment=increment,
-        textvariable=spinbox_variable,
-        width=spinbox_width,
-        justify="center",
-    )
-    spinbox.grid(row=row, column=1, sticky="w", pady=5, padx=5)
-
-    return label, spinbox_variable, spinbox
-
-
-class SpinboxesFrame(ttk.LabelFrame):
-    def __init__(self, container, init_file_path: str, text: str):
-        super().__init__(container, text=text)
-
-        # Loads some configuration parameters regarding the layout of the widgets in the window
-        init = np.loadtxt(init_file_path, dtype=str, delimiter=",", skiprows=1)
-
-        # Configures columns of the window
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-
-        # Initializes the spinboxes arrays
-        self.variables = np.zeros(init.shape[0], dtype=object)
-        self.labels = np.zeros(init.shape[0], dtype=object)
-        self.spinboxes = np.zeros(init.shape[0], dtype=object)
-
-        # Creates and configures each row containing a spinbox
-        for i in range(init.shape[0]):
-            self.grid_rowconfigure(i, weight=1)
-            self.labels[i], self.variables[i], self.spinboxes[i] = spinbox_row(
-                self,
-                init[i, 0],
-                float(init[i, 1]),
-                float(init[i, 1]),
-                float(init[i, 2]),
-                float(init[i, 3]),
-                i,
-                int(init[i, 4]),
-                int(init[i, 5]),
-            )
 
 
 class LabeledSpinbox:
@@ -97,6 +30,7 @@ class LabeledSpinbox:
         rowspan: int = 1,
         columnspan: int = 1,
         spinbox_width: float = 10,
+        sticky=None,
     ):
         self.frame = tk.Frame(container)
         for i in range(1):
@@ -135,6 +69,7 @@ class LabeledSpinbox:
             columnspan=columnspan,
             padx=5,
             pady=5,
+            sticky=sticky,
         )
 
     def get(self):
@@ -156,6 +91,7 @@ class LabeledCombobox:
         value_list: list = None,
         default_value: Union[str, int] = "",
         combobox_width: float = 15,
+        sticky=None,
     ):
         self.frame = tk.Frame(container)
         for i in range(1):
@@ -192,6 +128,7 @@ class LabeledCombobox:
             columnspan=columnspan,
             padx=5,
             pady=5,
+            sticky=sticky,
         )
 
     def get(self):
@@ -213,6 +150,7 @@ class LabeledEntry:
         column: int = 0,
         rowspan: int = 1,
         columnspan: int = 1,
+        sticky=None,
     ):
         self.frame = tk.Frame(container)
         for i in range(1):
@@ -234,6 +172,7 @@ class LabeledEntry:
             columnspan=columnspan,
             padx=5,
             pady=5,
+            sticky=sticky,
         )
 
     def get(self):
@@ -254,6 +193,7 @@ class Checkbox:
         columnspan: int = 1,
         default_value: bool = False,
         command: callable = None,
+        sticky=None,
     ):
         self.var = tk.BooleanVar(container, default_value)
         self.checkbutton = ttk.Checkbutton(
@@ -271,6 +211,7 @@ class Checkbox:
             columnspan=columnspan,
             padx=5,
             pady=5,
+            sticky=sticky,
         )
 
     def get(self):
