db6e089bd2a0a9342ca16c7e84f8c46b7c1cbb17
José Grilo <jose.grilo@research.fchampalimaud.org>
2025-01-28 10:39:10 +0000

    setup.ps1 updated

--- setup.ps1
+++ setup.ps1
@@ -9,11 +9,11 @@ if (!(Test-Path ".\assets")) {
 }
 
 if (!(Test-Path ".\assets\toSoundCard.exe")) {
-    Invoke-WebRequest "https://github.com/fchampalimaud/cdc-speaker-calibration/releases/download/v0.2.0-alpha/toSoundCard.exe" -OutFile ".\assets\toSoundCard.exe"
+    Invoke-WebRequest "https://github.com/fchampalimaud/cdc-speaker-calibration/releases/download/v0.3.0-alpha/toSoundCard.exe" -OutFile ".\assets\toSoundCard.exe"
 }
 
 if (!(Test-Path ".\assets\LibUsbDotNet.dll")) {
-    Invoke-WebRequest "https://github.com/fchampalimaud/cdc-speaker-calibration/releases/download/v0.2.0-alpha/LibUsbDotNet.dll" -OutFile ".\assets\LibUsbDotNet.dll"
+    Invoke-WebRequest "https://github.com/fchampalimaud/cdc-speaker-calibration/releases/download/v0.3.0-alpha/LibUsbDotNet.dll" -OutFile ".\assets\LibUsbDotNet.dll"
 }
 
 # Check if uv is already installed
