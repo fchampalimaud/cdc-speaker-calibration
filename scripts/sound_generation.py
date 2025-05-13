10f6bcb9cac38a5d437951eadc13b5396aeb1b82
José Grilo <jose.grilo@research.fchampalimaud.org>
2025-05-13 17:33:25 +0100

    Refactor: update sound generation script in order to make it use new code

--- scripts/sound_generation.py
+++ scripts/sound_generation.py
@@ -1,52 +1,7 @@
-from speaker_calibration.protocol.generate_sound import generate_noise
+import os
+
 import numpy as np
-
-
-def create_sound_file(
-    signal_left: np.ndarray,
-    signal_right: np.ndarray,
-    filename: str,
-    speaker_side: str = "both",
-):
-    """
-    Creates the .bin sound file to be loaded to the Harp Sound Card.
-
-    Parameters
-    ----------
-    signal_left : numpy.ndarray
-        the signal to be written to the .bin file that is going to be played by the left speaker.
-    signal_right : numpy.ndarray
-        the signal to be written to the .bin file that is going to be played by the right speaker.
-    filename : str
-        the name of the .bin file.
-    speaker_side : str
-        whether the sound plays in both speakers or in a single one. Possible values: "both", "left" or "right.
-    """
-    # Transforms the signal from values between -1 to 1 into 24-bit integers
-    amplitude24bits = np.power(2, 31) - 1
-
-    if speaker_side == "both":
-        wave_left = amplitude24bits * signal_left
-        wave_right = amplitude24bits * signal_right
-    elif speaker_side == "left":
-        wave_left = amplitude24bits * signal_left
-        wave_right = 0 * signal_right
-    elif speaker_side == "right":
-        wave_left = 0 * signal_left
-        wave_right = amplitude24bits * signal_right
-    else:
-        raise ValueError(
-            'speaker_side value should be "both", "left" or "right" instead of "%s"'
-            % speaker_side
-        )
-
-    # Groups the signals to be played in the left and right channels/speakers in a single array
-    stereo = np.stack((wave_left, wave_right), axis=1)
-    wave_int = stereo.astype(np.int32)
-
-    # Writes the sound to the .bin file
-    with open(filename, "wb") as f:
-        wave_int.tofile(f)
+from speaker_calibration.sound import create_sound_file, white_noise
 
 
 def upload_sound(
@@ -59,7 +14,7 @@ def upload_sound(
     ild: float = 0,
     fs: int = 192000,
     filename: str = "sound.bin",
-    soundcard_index: int = 2,
+    soundcard_index: int = None,
 ):
     if soundcard_index < 2 and soundcard_index > 31:
         raise (ValueError("soundcard_index must be between 2 and 31"))
@@ -78,14 +33,33 @@ def upload_sound(
         db_right = abl + ild / 2
         attenuation_right = 10 ** ((db_right - fit_right[1]) / fit_right[0])
 
-    signal_left = generate_noise(
-        fs, duration, amplification=attenuation_left, calibration_factor=cf_left
+    signal_left = white_noise(
+        duration,
+        fs,
+        amplification=attenuation_left,
+        freq_min=5000,
+        freq_max=20000,
+        inverse_filter=cf_left,
     )
-    signal_right = generate_noise(
-        fs, duration, amplification=attenuation_right, calibration_factor=cf_right
+    signal_right = white_noise(
+        duration,
+        fs,
+        amplification=attenuation_right,
+        freq_min=5000,
+        freq_max=20000,
+        inverse_filter=cf_right,
     )
 
     create_sound_file(signal_left, signal_right, filename)
+    if soundcard_index is not None:
+        os.system(
+            "cmd /c .\\assets\\toSoundCard.exe "
+            + filename
+            + " "
+            + str(soundcard_index)
+            + " 0 "
+            + str(fs)
+        )
 
 
 for i in range(5):
@@ -99,4 +73,5 @@ for i in range(5):
         ild=0,
         fs=192000,
         filename="sound" + str(i) + ".bin",
+        soundcard_index=(2 * i + 2),
     )
